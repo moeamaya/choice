1cd99a1532b2875d61efdee28a84641bd49603af
Moe Amaya <moe@monograph.io>
2021-11-03 12:23:08 -0700

    Remove ability for input zoom

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Fin</title>
         <meta name="description" content="Drawdown calculator" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
