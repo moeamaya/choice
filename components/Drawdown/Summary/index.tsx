87a836f9617d1e59a9c475924f846da1c6ba057d
Moe Amaya <moe@monograph.io>
2021-11-04 10:20:22 -0700

    Widen summary

--- components/Drawdown/Summary/index.tsx
+++ components/Drawdown/Summary/index.tsx
@@ -9,13 +9,13 @@ type Props = {
 const StyledSummary = styled('div', {
   display: `flex`,
   position: `fixed`,
-  left: 0,
+  left: `-0.25rem`,
   bottom: `1rem`,
   margin: `0 1rem`,
   background: `#fff`,
   border: `1px solid rgba(239, 241, 244, 1)`,
   borderRadius: `2px`,
-  width: `calc(100% - 2rem)`,
+  width: `calc(100% - 1.5rem)`,
   padding: `1rem`,
   boxShadow: `0px 0px 1px 0px rgba(0, 0, 0, 0.1),
     0px 6px 6px -6px rgba(0, 0, 0, 0.16)
