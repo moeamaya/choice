4c8c337c7d70cdb5e885989c431d79a4aa5122a7
Moe Amaya <moe@monograph.io>
2021-12-31 14:35:43 -0800

    Use switch statement

--- components/Drawdown/index.tsx
+++ components/Drawdown/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import { OptionType } from '../Choice/Options';
 import Radio from './Radio';
 
 import Years from './Years';
 
 const Drawdown = () => {
-  const [calculator, setCalculator] = useState({
+  const [selected, setSelected] = useState({
     value: 'years',
     label: 'Years',
   });
@@ -13,23 +14,36 @@ const Drawdown = () => {
   const [rate, setRate] = useState({ value: '0.07', label: '☒ 7% (Average)' });
   const [income, setIncome] = useState({ value: '30000', label: '$ 30,000' });
 
-  // const years = parseFloat(time.value);
+  const years = parseFloat(time.value);
   const interest = parseFloat(rate.value);
   const draw = parseFloat(income.value);
 
+  const Calculator = ({ selected }: { selected: OptionType }) => {
+    const selectedValue = selected.value;
+
+    switch (selectedValue) {
+      case 'years':
+        return (
+          <Years
+            amount={amount}
+            setAmount={setAmount}
+            income={income}
+            setIncome={setIncome}
+            rate={rate}
+            setRate={setRate}
+            draw={draw}
+            interest={interest}
+          />
+        );
+      default:
+        return <div>Error</div>;
+    }
+  };
+
   return (
     <>
-      <Radio selected={calculator} setSelected={setCalculator} />
-      <Years
-        amount={amount}
-        setAmount={setAmount}
-        income={income}
-        setIncome={setIncome}
-        rate={rate}
-        setRate={setRate}
-        draw={draw}
-        interest={interest}
-      />
+      <Radio selected={selected} setSelected={setSelected} />
+      <Calculator selected={selected} />
     </>
   );
 };
